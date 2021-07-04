44f3f200d0b512fa211bc137f258f7e358273224
Erika Espejo <erikaespejo95@gmail.com>
2021-07-04 13:02:43 -0500

    counter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+    this.changeCharacters = this.changeCharacters.bind(this);
+  }
+
+  changeCharacters = (event) => {
+    this.setState({
+      counter: event.target.value.length,
+    });
+  };
+
   render() {
+    const { counter } = this.state;
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea
+          rows="3"
+          onChange={this.changeCharacters}
+          ref={this.textarea}
+        ></textarea>
+        <div className="counter">{counter}</div>
       </div>
     );
   }
